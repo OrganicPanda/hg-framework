715fb15367d98e225088447dce113f7d9ab00072
Harry Hogg <harry@hogg.io>
2015-06-07 13:05:01 +0100

    Improved sidebar and menu

--- app/components/sidebar/sidebar.js
+++ app/components/sidebar/sidebar.js
@@ -14,4 +14,49 @@ angular.module( 'hg.components.sidebar', [
       replace: true,
       templateUrl: '/dist/components/sidebar/sidebar.html'
     };
+  })
+
+  /**
+   *
+   */
+  .directive('hgSidebarMenu', function() {
+    return {
+      replace: true,
+      templateUrl: '/dist/components/sidebar/menu.html',
+      controller: 'HgSidebarMenuCtrl'
+    };
+  })
+
+  /**
+   *
+   */
+  .controller('HgSidebarMenuCtrl', function($scope, $state) {
+    /**
+     *
+     */
+    $scope.menu = null;
+
+    /**
+     *
+     */
+    $scope.levelUp = function() {
+      $scope.menu = $state.current.data.parent;
+    };
+
+    /**
+     *
+     */
+    $scope.levelDown = function(state) {
+      $scope.menu = state;
+    };
+
+
+    //
+    $scope.$watch(function() {
+      return $state.current;
+    }, function(newState, oldState) {
+      if (newState !== oldState && newState.data && newState.data.siblings) {
+        $scope.menu = newState;
+      }
+    });
   });
